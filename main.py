64faa8d6af46b8fb444732d3c82fca2004bb640c
iomega11 <nicolas.andre@master-developpement-logiciel.fr>
2019-11-27 16:03:17 +0100

    token env

--- main.py
+++ main.py
@@ -53,8 +53,9 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-#TOKEN = os.environ('TOKEN')
-TOKEN = 'NjQ5MTQxODY0NzIwNzYwODYy.Xd5qUA.jgMd3_shOzJl8ybkBgFcjn-exPc'
+TOKEN = os.environ.get('TOKEN')
+#TOKEN = 'NjQ5MTQxODY0NzIwNzYwODYy.Xd5qUA.jgMd3_shOzJl8ybkBgFcjn-exPc'
+#TOKEN = 'NjQ5MTQxODY0NzIwNzYwODYy.Xd6PzQ.3NoCs81Gqd8QX3sJ_tOm4eNw_tA'
 #TOKEN = 'NjQ5MTQxODY0NzIwNzYwODYy.Xd6NEw.qb2i8TPUkTWoNPaPRnlCWuPVceQ'
 
 print("token : ",TOKEN)
