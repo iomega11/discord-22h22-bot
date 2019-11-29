a7b8edbd6529dec983fa4332497b94c0beb77518
Nicolas ANDRE <glagla255@free.fr>
2019-11-29 22:02:47 +0100

    correction identification ownerid dans exec et logout

--- main.py
+++ main.py
@@ -68,7 +68,8 @@ async def on_message(message):
         ignored = False
         await message.channel.send(msg)
     elif ( (message.content.startswith('.close') or message.content.startswith('.stop') or message.content.startswith('.logout'))):
-        print(message.author.id," == ", OWNERID," ? ", (message.author.id == OWNERID))
+        print(message.author.id," == ", OWNERID," ? ", (str(message.author.id) == OWNERID))
+        
         ignored = False
         conn.commit()
         conn.close()
